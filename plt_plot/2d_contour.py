6f4f232f1a56494d1660f6746bdcf8a6d07c2038
Jian Huang <huangjianhuster@gmail.com>
2024-07-16 22:30:45 -0400

    update 2d plot

--- plt_plot/2d_contour.py
+++ plt_plot/2d_contour.py
@@ -20,16 +20,22 @@ def contour_plot(dat, bins=200, colormap=None, color="#00BFFF", contour_levels=N
 
     # processing raw data
     heatmap, xedges, yedges = np.histogram2d(dat[0,:], dat[1,:], bins=[bins, bins], density=True)
+    extent = [xedges[0], xedges[-1],yedges[0], yedges[-1]]
 
     # plot
     fig, axs = plt.subplots(1, 1, figsize=(6, 6)) # ,  subplot_kw=dict(aspect='equal'))
-    im = axs.imshow(heatmap.T, origin='lower', extent=[xedges[0], xedges[-1],\
-                    yedges[0], yedges[-1]], cmap=custom_cmap, norm=mpl.colors.LogNorm(),\
+    im = axs.imshow(heatmap.T, origin='lower', extent=extent, cmap=custom_cmap, norm=mpl.colors.LogNorm(),\
                     zorder=2, alpha=0.8)
-    cax = fig.add_axes([axs.get_position().x1+0.02, axs.get_position().y0, 0.04, axs.get_position().height])
+
+    # set xlim and ylim
+    # axs.set_xlim(x_min, x_max)
+    # axs.set_ylim(y_min, y_max)
+
+    cax = fig.add_axes([axs.get_position().x1+0.01, axs.get_position().y0, 0.02, axs.get_position().height])
     colorbar = fig.colorbar(im, cax=cax)
-    colorbar.set_label('Density') #, labelpad=20)
-    
+    # colorbar.set_label('Density') #, labelpad=20)
+    colorbar.ax.tick_params(labelsize=8)
+
     # add contour
     # mask the heatmap since there are lots of zeros
     heatmap = np.ma.masked_where(heatmap == 0, heatmap)
@@ -41,24 +47,16 @@ def contour_plot(dat, bins=200, colormap=None, color="#00BFFF", contour_levels=N
 
     # exponential decay of the linewidth
     linewidths = np.logspace(np.log10(0.1), np.log10(3), len(contour_levels))
-    contours = axs.contour(heatmap.T, extent=[xedges[0], xedges[-1],\
-                        yedges[0], yedges[-1]], levels=contour_levels, \
+    contours = axs.contour(heatmap.T, extent=extent, levels=contour_levels, \
                             colors=color, linewidths=linewidths, zorder=5)
 
     # Add contour levels as lines on colorbar
-    cb_ticks = colorbar.ax.get_yticks()
-    for tick,thickiness in zip(contour_levels, linewidths):
-        colorbar.ax.axhline(tick, color=color, linestyle='-', linewidth=thickiness)
-        colorbar.ax.text(3.5, tick, f'{tick:.1e}', color='b', ha='right', va='center', fontsize=8)
-
-    # axs.set_xlabel(r'$\phi$')
-    # axs.set_ylabel(r'$\psi$')
-    # axs.set_xlim([-180, 180])
-    # axs.set_ylim([-180, 180])
-    # axs.set_xticks(np.arange(-180, 240, 60))
-    # axs.set_yticks(np.arange(-180, 240, 60))
-    axs.grid(zorder=0)
-    plt.tight_layout()
+    # cb_ticks = colorbar.ax.get_yticks()
+    # for tick,thickiness in zip(contour_levels, linewidths):
+    #     colorbar.ax.axhline(tick, color=color, linestyle='-', linewidth=thickiness)
+    #     colorbar.ax.text(3.5, tick, f'{tick:.1e}', color='b', ha='right', va='center', fontsize=8)
+    # axs.grid(zorder=0)
+    # plt.tight_layout()
     if save_path:
         plt.savefig(save_path)
     plt.show()
