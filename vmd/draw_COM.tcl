45e193589795c8bcace9abace53549f409147292
jianhuang <jianhuang@umass.edu>
2025-04-26 16:32:24 -0400

    update vmd scripts

--- vmd/draw_COM.tcl
+++ vmd/draw_COM.tcl
@@ -1,19 +1,28 @@
+# Global variable to store the ID of the sphere
+set sphere_id ""
 
 # Set up a callback procedure to update the sphere position during animation
 proc update_sphere_position {} {
+    global sphere_id
+
     # global vmd_frame
     set selection [atomselect top "chain J and resname POPI and name C12 O2 C13 O3 C14 O4 P4 OP42 OP43 OP44 C15 O5 P5 OP52 OP53 OP54 C16 O6 C11 P O13 O14 O12 O11"]
     set com [measure center $selection weight mass]
-    draw delete all
-    draw color red
-    draw sphere $com radius 1.0 resolution 50
+    
+    # Delete the old sphere
+    if {$sphere_id ne ""} {
+        draw delete $sphere_id
+    }
+
+    draw color green
+    set sphere_id [draw sphere $com radius 1.0 resolution 50]
 
     # draw reference     
-    draw color blue
-    draw sphere {50 50 72} radius 2 resolution 50
-    draw sphere {50 90 72} radius 2 resolution 50
-    draw sphere {90 90 72} radius 2 resolution 50
-    draw sphere {90 50 72} radius 2 resolution 50
+    # draw color blue
+    # draw sphere {50 50 72} radius 2 resolution 50
+    # draw sphere {50 90 72} radius 2 resolution 50
+    # draw sphere {90 90 72} radius 2 resolution 50
+    # draw sphere {86.2 92.7 67.9} radius 1.0 resolution 50
     # puts "drawing..."
 }
 
---
+++ vmd/draw_label.tcl
@@ -0,0 +1,97 @@
+# First, define a global list to store graphics IDs (to delete them later)
+set text_labels {}
+
+# Three letter to One letter mapping
+set aa1letter {
+    ALA A
+    ARG R
+    ASN N
+    ASP D
+    CYS C
+    GLN Q
+    GLU E
+    GLY G
+    HIS H
+    HIE H
+    HSE H
+    HID H
+    ILE I
+    LEU L
+    LYS K
+    MET M
+    PHE F
+    PRO P
+    SER S
+    THR T
+    TRP W
+    TYR Y
+    VAL V
+}
+
+
+
+# Define a procedure to update the labels
+proc update_labels {} {
+    global text_labels
+    global aa1letter
+
+    # Delete old labels
+    foreach id $text_labels {
+        graphics top delete $id
+    }
+    set text_labels {}
+
+    # Select contacting residues (adjust your ligand selection if needed)
+    set contacting [atomselect top "(same residue as (protein and within 4 of (resname POPI  and chain J and  name  C12 O2 C13 O3 C14 O4 P4 2OP4 3OP4 4OP4 C15 O5 P5 2OP5 3OP5 4OP5 C16 O6 C11 P O13 O14 O12 O11 ))) and resid 450 to 750 and name CA"]
+    $contacting update
+
+    # Get list of contacting residues
+    set reslist [$contacting get {resname resid x y z}]
+    # puts $reslist
+    
+    # Loop through all residues and labeling them
+    foreach res $reslist {
+
+        lassign $res resname resid x y z
+
+        # label shift
+        set x_shift [expr {$x + 1}]
+        set y_shift [expr {$y + 1}]
+        set z_shift [expr {$z + 1}]
+
+        # map to one letter
+        set clean_resname [string trim $resname]
+
+        if {[dict exists $aa1letter $clean_resname]} {
+            set oneletter [dict get $aa1letter $clean_resname]
+        } else {
+            set oneletter "X"  ;# fallback for unknown residue
+        }
+
+        set label "${oneletter}${resid}"
+
+        # Three letter version
+        # set label "${resname}${resid}"
+
+        # Set label color blue (ColorID 0 = blue by default)
+        graphics top color 0
+        
+        # Draw the text
+        set id [graphics top text "$x_shift $y_shift $z_shift" $label size 0.7 thickness 2.5]
+        lappend text_labels $id
+    }
+
+    # Clean up selection
+    $contacting delete
+}
+
+proc label_trace {args} {
+    update_labels
+}
+
+proc remove_labels {} {
+    graphics top delete all
+}
+
+# Attach the update to frame changes
+trace variable vmd_frame w label_trace
