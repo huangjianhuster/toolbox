a395e9ba0a4e0842f10fdaeaa230f8a8d5064490
jianhuang <jianhuang@umass.edu>
2025-05-02 13:19:16 -0400

    modify

--- vmd/time.tcl
+++ vmd/time.tcl
@@ -1,28 +1,32 @@
 ## for time
-
+set text_time {}
 
 proc enabletrace {} {
-  global vmd_frame;
+  global vmd_frame
+  global text_time
   trace variable vmd_frame([molinfo top]) w drawcounter
 }
 
 proc disabletrace {} {
-  global vmd_frame;
+  global vmd_frame
   trace vdelete vmd_frame([molinfo top]) w drawcounter
 }
 
 proc drawcounter { name element op } {
-  global vmd_frame;
+  global vmd_frame
+  global text_time
 
-  draw delete all
-  # puts "callback!"
+  # Delete old labels
+  foreach id $text_time {
+      graphics top delete $id
+  }
+  set text_time {}
+
   draw color red
   set timeperframe 0.05
   set time [format "%8.2f ns" [expr $vmd_frame([molinfo top]) * $timeperframe]]
-  draw text {50 65 130} "$time" size 2 thickness 4
+  set id [graphics top text {50 65 130} "$time" size 2 thickness 4]
+  lappend text_time $id
 }
 
 enabletrace
-
-Type:   enabletrace
-
